2d801a607ab87710ec9e48e0d8a568ef70b1ab49
cxn <chenxinan@xiyun.com.cn>
2018-11-07 09:36:48 +0800

    添加测试测试类-bigdecimal

---
+++ src/main/java/com.xiyun.cxn.java.base/constant/BigdecimalTest.java
@@ -0,0 +1,15 @@
+package constant;
+
+/**
+ * @program javaany_
+ * @description: bigdecimal start..
+ * @author: cxn
+ * @create: 2018/11/07 09:27
+ */
+public class BigdecimalTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
