01d6133168ac1427fb9c863106259cd3635e7b44
JAVA-陈喜楠 <chenxinan@xiyun.com.cn>
2020-12-17 10:46:16 +0800

    添加测试

--- src/main/java/com/xiyun/cxn/java/lambda/ListToMapTest.java
+++ src/main/java/com/xiyun/cxn/java/lambda/ListToMapTest.java
@@ -2,7 +2,7 @@ package com.xiyun.cxn.java.lambda;
 
 import com.alibaba.fastjson.JSONObject;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,70 +17,73 @@ public class ListToMapTest {
 
 
     public static void main(String[] args) {
-        ArrayList<Merchant> merchants = new ArrayList<>();
+        List<String> runChannelList = Arrays.asList("wx");
+        System.out.println(JSONObject.toJSONString(runChannelList));
 
-        Merchant merchant = new Merchant();
-        merchant.setGroupId("123");
-        merchant.setGroupName("xiaohong");
-        merchant.setCount(50);
-
-
-        Merchant merchant1 = new Merchant();
-        merchant1.setGroupId("123");
-        merchant1.setGroupName("xiaohong");
-        merchant1.setCount(150);
-
-        Merchant merchant2 = new Merchant();
-        merchant2.setGroupId("234");
-        merchant2.setGroupName("xiaoli");
-        merchant2.setCount(200);
-
-        merchants.add(merchant);
-        merchants.add(merchant1);
-        merchants.add(merchant2);
-
-
-        List<Merchant> collect = merchants.stream().filter(a -> a.getCount() == 200 || a.getCount() == 150).collect(Collectors.toList());
-
-        System.out.println("111"+JSONObject.toJSONString(collect));
-
-        //List<Merchant>  collect = merchants.stream().collect(Collectors.groupingBy();
-
-
-        //Map<String, String> idNameMap = getIdNameMap(merchants);
-        //merchants.stream().filter((t1,t2)->{t1.getGroupId().equals(t2.getGroupId())})
-
-
-//        Map<String, String> groupIdNameMap =
-//                merchants.stream().collect(Collectors.toMap(Merchant::getGroupId,
-//                        groupConfig -> groupConfig.getGroupName(), (key1, key2) -> key1));
+//        ArrayList<Merchant> merchants = new ArrayList<>();
+//
+//        Merchant merchant = new Merchant();
+//        merchant.setGroupId("123");
+//        merchant.setGroupName("xiaohong");
+//        merchant.setCount(50);
 //
 //
-//        System.out.println(groupIdNameMap.toString());
-
-
-
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        list.forEach(i->{
-            try{
-                if(i.intValue() == 1){
-                    return;
-                }
-
-                System.out.println(i);
-                System.out.println(123);
-            }catch (Exception e){
-
-            }
-
-        });
-        System.out.println("haha");
+//        Merchant merchant1 = new Merchant();
+//        merchant1.setGroupId("123");
+//        merchant1.setGroupName("xiaohong");
+//        merchant1.setCount(150);
+//
+//        Merchant merchant2 = new Merchant();
+//        merchant2.setGroupId("234");
+//        merchant2.setGroupName("xiaoli");
+//        merchant2.setCount(200);
+//
+//        merchants.add(merchant);
+//        merchants.add(merchant1);
+//        merchants.add(merchant2);
+//
+//
+//        List<Merchant> collect = merchants.stream().filter(a -> a.getCount() == 200).filter(a->  a.getGroupId().equals("234")).collect(Collectors.toList());
+//
+//        System.out.println("111"+JSONObject.toJSONString(collect));
+//
+//        //List<Merchant>  collect = merchants.stream().collect(Collectors.groupingBy();
+//
+//
+//        //Map<String, String> idNameMap = getIdNameMap(merchants);
+//        //merchants.stream().filter((t1,t2)->{t1.getGroupId().equals(t2.getGroupId())})
+//
+//
+////        Map<String, String> groupIdNameMap =
+////                merchants.stream().collect(Collectors.toMap(Merchant::getGroupId,
+////                        groupConfig -> groupConfig.getGroupName(), (key1, key2) -> key1));
+////
+////
+////        System.out.println(groupIdNameMap.toString());
+//
+//
+//
+//        List<Integer> list = new ArrayList<Integer>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//
+//        list.forEach(i->{
+//            try{
+//                if(i.intValue() == 1){
+//                    return;
+//                }
+//
+//                System.out.println(i);
+//                System.out.println(123);
+//            }catch (Exception e){
+//
+//            }
+//
+//        });
+//        System.out.println("haha");
+//    }
     }
-
 
     public static  Map<String, String> getIdNameMap(List<Merchant> ms) {
         return ms.stream().collect(Collectors.toMap(Merchant::getGroupId, Merchant::getGroupName));
